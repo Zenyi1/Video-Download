0582598bb880674115387927dc94111f59bee5f9
Carlos Zenyi Gomez Aryoshi <zenyigom@hotmail.com>
2023-07-09 19:36:11 -0600

    changes libraries to bypass size limits

--- api/download.js
+++ api/download.js
@@ -1,32 +1,20 @@
-const ytdl = require('ytdl-core');
-const fs = require('fs');
+const ytdl = require('ytdl-core-discord');
 
 module.exports = async (req, res) => {
   try {
     const { URL } = req.query;
 
-    // Download the video and save it to a temporary file
     const videoInfo = await ytdl.getInfo(URL);
     const format = ytdl.chooseFormat(videoInfo.formats, { quality: 'highest' });
-    const videoReadableStream = ytdl(URL, { format });
+    const stream = ytdl(URL, { format });
 
-    const tempFilePath = '/tmp/video.mp4'; // Temporary file path
+    res.setHeader('Content-Type', 'video/mp4');
+    res.setHeader('Content-Disposition', `attachment; filename="video.mp4"`);
 
-    await new Promise((resolve, reject) => {
-      const writeStream = fs.createWriteStream(tempFilePath);
-      videoReadableStream.pipe(writeStream);
+    await stream.pipe(res);
 
-      videoReadableStream.on('end', resolve);
-      writeStream.on('error', reject);
-    });
-
-    // Stream the video file to the response
-    const videoReadStream = fs.createReadStream(tempFilePath);
-    videoReadStream.pipe(res);
-
-    // Clean up the temporary file after streaming is complete
-    videoReadStream.on('end', () => {
-      fs.unlinkSync(tempFilePath);
+    stream.on('end', () => {
+      res.end();
     });
   } catch (error) {
     console.error('Error occurred:', error);
