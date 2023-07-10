a77719c8085161e8d3452f1fddeaac6d59955bc0
Carlos Zenyi Gomez Aryoshi <zenyigom@hotmail.com>
2023-07-09 19:02:07 -0600

    removed localhost server

--- index.js
+++ index.js
@@ -4,9 +4,7 @@ const ytdl = require('ytdl-core');
 const app = express();
 
 app.use(cors());
-app.listen(4000, () => {
-    console.log('Server Works !!! At port 4000');
-});
+
 app.get('/download', (req,res) => {
     var URL = req.query.URL;
     res.header('Content-Disposition', 'attachment; filename="video.mp4"');
--- script.js
+++ script.js
@@ -6,5 +6,5 @@ convertBtn.addEventListener('click', () => {
     sendURL(URLinput.value);
 });
 function sendURL(URL) {
-    window.location.href = `http://localhost:4000/download?URL=${URL}`;
+    window.location.href = `https://video-download-indol.vercel.app/download?URL=${URL}`;
 }
