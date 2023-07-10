1cbe6afa208cd0407c690b91de0e81e10421cec1
Carlos Zenyi Gomez Aryoshi <zenyigom@hotmail.com>
2023-07-09 19:32:39 -0600

    fixed issue with large files so that they go to tmp/

--- api/download.js
+++ api/download.js
@@ -1,7 +1,35 @@
 const ytdl = require('ytdl-core');
+const fs = require('fs');
 
 module.exports = async (req, res) => {
-  const { URL } = req.query;
-  res.setHeader('Content-Disposition', 'attachment; filename="video.mp4"');
-  ytdl(URL, { format: 'mp4' }).pipe(res);
+  try {
+    const { URL } = req.query;
+
+    // Download the video and save it to a temporary file
+    const videoInfo = await ytdl.getInfo(URL);
+    const format = ytdl.chooseFormat(videoInfo.formats, { quality: 'highest' });
+    const videoReadableStream = ytdl(URL, { format });
+
+    const tempFilePath = '/tmp/video.mp4'; // Temporary file path
+
+    await new Promise((resolve, reject) => {
+      const writeStream = fs.createWriteStream(tempFilePath);
+      videoReadableStream.pipe(writeStream);
+
+      videoReadableStream.on('end', resolve);
+      writeStream.on('error', reject);
+    });
+
+    // Stream the video file to the response
+    const videoReadStream = fs.createReadStream(tempFilePath);
+    videoReadStream.pipe(res);
+
+    // Clean up the temporary file after streaming is complete
+    videoReadStream.on('end', () => {
+      fs.unlinkSync(tempFilePath);
+    });
+  } catch (error) {
+    console.error('Error occurred:', error);
+    res.status(500).send('Internal Server Error');
+  }
 };
